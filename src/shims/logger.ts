e579e6df7d1e1c2640823ccac3cd5672ada10489
Misty <gyc990326@gmail.com>
2022-04-24 03:48:19 +0800

    [shims] add request-related logging

--- src/shims/logger.ts
+++ src/shims/logger.ts
@@ -1,9 +1,16 @@
+import { RequestShim } from "./request"
+
 /* eslint-disable @typescript-eslint/no-unused-vars */
 export const doLog = function(msg: any): void {
     throw new Error("unimplemented shim function: " + arguments.callee.name)
 }
 
 export default {
+    reqlog(req: RequestShim, msg: any) {
+        req.logs.push("" + msg)
+        doLog("[ReqLog] " + msg)
+    },
+
     info(msg: any) {
         doLog("[INFO] " + msg)
     },
