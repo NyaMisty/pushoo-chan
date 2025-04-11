38e4a3dae270c9a3e49fe1fdea4be724d5c08d22
Misty <gyc990326@gmail.com>
2025-04-11 11:35:53 +0800

    [shims] support retry during sending notify

--- src/shims/push.ts
+++ src/shims/push.ts
@@ -4,6 +4,70 @@ import { ChannelConfig, getRealChannelList } from "@models/channels";
 
 import pushoo from 'pushoo';
 
+const MAX_RETRY = 5;
+const RETRY_INTERVAL = 3.0; // 3s per round
+
+function shouldRetry(msg: string) {
+    // const msg = err.message
+    if (msg.includes("Network Error")) {
+        return true
+    }
+    if (msg.includes("timeout")) {
+        return true
+    }
+    if (msg.includes("failed with status code")) {
+        const statusCode = parseInt(msg.split("status code")[1])
+        if (statusCode >= 500) {
+            return true
+        }
+    }
+    return false
+}
+
+async function pushOneChanInternal(chan: ChannelConfig, content: string, title: string | undefined) {
+    const result = await pushoo(chan.type, {
+        token: chan.token,
+        content: content || '',
+        title,
+    });
+    if (result.error) {
+        const err: Error = result.error
+        logger.err(err)
+        // return "push error: " + err.toString();
+        return { status: "error", msg: err.toString() };
+    } else {
+        return { status: "success", msg: JSON.stringify(result) };
+    }
+}
+
+async function sleep(duration: number) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(null);
+        }, duration);
+    });
+}
+
+async function pushOneChan(chan: ChannelConfig, content: string, title: string | undefined) {
+    let retMsg = '';
+    for (let i = 0; i < MAX_RETRY; i++) {
+        const {status, msg} = await pushOneChanInternal(chan, content, title)
+        if (status == "success") {
+            retMsg += `push success (round ${i+1}): ${msg}`
+            break
+        } else if (status == 'error') {
+            retMsg += `push error (round ${i+1}): ${msg}`
+            if (!shouldRetry(msg)) {
+                retMsg += '\n'
+                retMsg += `push failed because not retriable...`
+            }
+            await sleep(RETRY_INTERVAL)
+        }
+        retMsg += '\n'
+    }
+    return retMsg
+}
+
 export async function pushByChanString(
     channame: string | undefined, // channel names splitted by comma
     _title: string | undefined,
@@ -42,18 +106,7 @@ export async function pushByChanString(
 
         return await Promise.all(chans.map(
             async (chan: ChannelConfig) => {
-                const result = await pushoo(chan.type, {
-                    token: chan.token,
-                    content: content || '',
-                    title,
-                });
-                if (result.error) {
-                    const err: Error = result.error
-                    logger.err(err)
-                    return "push error: " + err.toString();
-                } else {
-                    return "push success: " + JSON.stringify(result)
-                }
+                return await pushOneChan(chan, content, title)
             }
         ))
     } catch(e) {
